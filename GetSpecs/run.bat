a8b1b573f763214323f7adadb4ba3669af09fa67
razevortex <razevortex@googlemail.com>
2024-09-18 21:39:25 +0200

    Update run.bat

--- GetSpecs/run.bat
+++ GetSpecs/run.bat
@@ -1,10 +1,12 @@
 @echo off
-pushd %~dp0
-set dire=%CD%
-popd
+
+REM get dir of the script and set it to %mypath%
 for %%i in ("%~dp0.") do SET "mypath=%%~fi"
+
+REM set vars for ps script and the file to write to
 set "filepath=%mypath%\specs.json"
 set "script=%mypath%\GetHardwareInfo.ps1"
-echo "& '%script%' -path '%filepath%'"
-powershell.exe -Command "& '%script%' -path '%filepath%'"
-pause
+
+REM run the ps script with file path as arg
+start /b powershell.exe -noexit -ExecutionPolicy Bypass -Command "& '%script%' -path '%filepath%'"
+exit
